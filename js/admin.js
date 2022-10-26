28df8ba522f442e69400b6f8ddc38494f40b2fa6
Mateo <ememattyu12@gmail.com>
2022-10-25 23:36:49 -0600

    Update

--- js/admin.js
+++ js/admin.js
@@ -1,13 +1,13 @@
-const listadoAdmins = document.querySelector('.listadoAdmins') ,
-templateAdmin = document.querySelector('#templateAdmin').content;
+const listadoAdmins = document.querySelector('.listadoAdmins'),
+    templateAdmin = document.querySelector('#templateAdmin').content;
 
 
 const datosAdmin = async () => {
     try {
         const response = await fetch("json/data.json");
         const data = await response.json();
-    //    console.log(data);
-       mostrarHTML(data);
+        //    console.log(data);
+        mostrarHTML(data);
     } catch (error) {
         console.log(error);
     }
@@ -23,19 +23,16 @@ const mostrarHTML = (data) => {
     const fragment = document.createDocumentFragment();
     data.forEach(item => {
         const clone = templateAdmin.cloneNode(true);
-        clone.querySelector('.card-img-top').setAttribute('src', item.img);
+        clone.querySelector('.img-fluid').setAttribute('src', item.img);
         clone.querySelector('.name').textContent = item.nombres;
         clone.querySelector('.apellido').textContent = item.apellidos;
-        clone.querySelector('.btn-primary').dataset.id = item.idUser;
-        // clone.querySelector('.edad').textContent = item.edad;
-        // clone.querySelector('.genero').textContent = item.genero == "M" ? "Masculino" : item.genero == "F" ?"Fenenina" : item.genero;
-        // clone.querySelector('.casado').textContent = item.casado ? "SI" : "NO";
-        // clone.querySelector('.email').textContent = item.email;
-
-
-
-
-
+        clone.querySelector('.edad').textContent = item.edad;
+        clone.querySelector('.genero').textContent = item.genero == "M" ? "Masculino" : item.genero == "F" ? "Fenenina" : item.genero;
+        clone.querySelector('.casado').textContent = item.casado ? "SI" : "NO";
+        clone.querySelector('.email').textContent = item.email;
+        clone.querySelector('.telephone').textContent = item.telephone;
+        clone.querySelector('.address').textContent = item.address;
+        clone.querySelector('.fechaNaciminento').textContent = item.fechaNaciminento;
         // clone.querySelector('.btn-info').setAttribute('href', item.detailPageURL);
         fragment.appendChild(clone);
     });
--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ formulario.addEventListener('submit', e => {
     }
     input.classList.remove("is-invalid");
     load.classList.remove("d-none");
-
+    spinner.classList.add("d-none");
     // console.log(inputValue);
     requestApi(inputValue);
 });
@@ -50,6 +50,8 @@ const requestApi = async (products) => {
             toastr.error(`Error al cargar, el servidor respondió con un estado de ${response.status}`, `Error: ${response.status}`, {
                 "positionClass": "toast-bottom-full-width"
             });
+            load.classList.add("d-none");
+            spinner.classList.remove("d-none");
             return;
             //  throw new Error(`Error! status: ${response.status}`);
         }
@@ -64,8 +66,11 @@ const requestApi = async (products) => {
                 "positionClass": "toast-top-right"
             });
             load.classList.add("d-none");
+            spinner.classList.remove("d-none");
             return;
         }
+        load.classList.add("d-none");
+        spinner.classList.remove("d-none");
 
         // resultadosEcontrados = data.length
         resultadosEcontrados = Object.values(data).length;
@@ -93,9 +98,11 @@ const mostrarHTML = (data) => {
         clone.querySelector('.card-title').textContent = item.title;
         clone.querySelector('.lista').textContent = item.listPrice;
         clone.querySelector('.precioOrig').textContent = item.price;
-        // clone.querySelector('.totals').textContent = item.totalReviews;
         clone.querySelector('.totals').textContent = isNaN(contres) ? " 00.00" : contres;
         clone.querySelector('.btn-info').setAttribute('href', item.detailPageURL);
+        // clone.querySelector('.btn-info').addEventListener('click', () => {
+        //     console.log("ejecutando");
+        // });
         fragment.appendChild(clone);
     });
     listas.appendChild(fragment);
@@ -106,7 +113,7 @@ const mostrarHTML = (data) => {
 /*------------------------------------WITH LOAD AUTOMATIC OF ALL THE PRODUCTS---------------------------------------------------------- */
 document.addEventListener('DOMContentLoaded', () => {
     spinner.classList.remove("d-none");
-   // cargarData();
+    cargarData();
 });
 
 async function cargarData() {
@@ -131,9 +138,9 @@ async function cargarData() {
     }
 
     try {
-        const response = await fetch(`https://api.rainforestapi.com/request?api_key=${api_3}&type=search&amazon_domain=amazon.com&search_term=canon+camera`);
+        const response = await fetch(`https://api.rainforestapi.com/request?api_key=${api_3}&type=search&amazon_domain=amazon.com&search_term=*`);
         const data = await response.json();
-    console.log(data);
+        // console.log(data);
 
         if (response.status === 401) {
             toastr.error(`Error al cargar, el servidor respondió con un estado de ${response.status}`, `Error: ${response.status}`, {
@@ -143,6 +150,7 @@ async function cargarData() {
             return;
             //  throw new Error(`Error! status: ${response.status}`);
         }
+        spinner.classList.add("d-none");
 
         insertarHTML(data);
     } catch (error) {
@@ -188,17 +196,13 @@ const insertarHTML = (apis) => {
     listas.textContent = "";
     const fragment = document.createDocumentFragment();
     apis.search_results.forEach(item => {
-console.log(item);
-
-        // console.log(item.position);
-        // console.log(item.asin);
-        // if (!item.availability) {
-        //     console.log("NO existe");
-        // } else {
-        //     console.log("Existe");
+        // console.log(item);
+        // if (!item.price.value) {
+        //     console.log("Esta vacio");
+        //     return;
+        // }else{
+        //     console.log("NO esta vacio");
         // }
-        // console.log(item.price.value);
-        // console.log(item.prices[0].value);
 
         tipoDeCambioQuetzalGT = (parseFloat(item.price) * 7.80);
         contres = tipoDeCambioQuetzalGT.toFixed(3);
@@ -207,11 +211,11 @@ console.log(item);
         clone.querySelector('.card-img-top').setAttribute('src', item.image);
         clone.querySelector('.card-title').textContent = item.title;
         // clone.querySelector('.badge-pill').textContent = item.availability ? item.availability : "Agotado";
-        // clone.querySelector('.lista').textContent = item.listPrice;
-        // clone.querySelector('.precioOrig').textContent = item.price.value;
+        clone.querySelector('.lista').textContent = " 00.00";
+     clone.querySelector('.precioOrig').textContent = " 00.00";
         // // clone.querySelector('.totals').textContent = item.totalReviews;
-        // clone.querySelector('.totals').textContent = isNaN(contres) ? " 00.00" : contres;
-        // clone.querySelector('.btn-info').setAttribute('href', item.detailPageURL);
+        clone.querySelector('.totals').textContent = isNaN(contres) ? " 00.00" : contres;
+        clone.querySelector('.btn-info').setAttribute('href', item.link);
         fragment.appendChild(clone);
     });
     listas.appendChild(fragment);
@@ -244,8 +248,14 @@ function log(apis) {
 document.addEventListener("DOMContentLoaded", practica)*/
 
 
+
+botonWithAsin = document.querySelector('.vieww')
+
 /*------------------------------------POR ASIN----------------------------*/
 const formAsin = document.querySelector('.formAsin'),
+    templateAsin = document.querySelector('#templateAsin').content,
+    listadbody = document.querySelector('.listadbody'),
+    letrasValid = document.querySelector('.letrasValid'),
     inputAsin = document.querySelector('.inputAsin');
 
 formAsin.addEventListener('submit', e => {
@@ -253,13 +263,20 @@ formAsin.addEventListener('submit', e => {
     getByValue = inputAsin.value
     if (getByValue === "") {
         inputAsin.classList.add("is-invalid");
-        toastr.error('Por favor, Ingresa el ASIN producto', 'Error', {
+        letrasValid.classList.add('active')
+        letrasValid.classList.remove('verde')
+        toastr.error('Por favor, Ingresa el ASIN del producto', 'Error', {
             "positionClass": "toast-top-right"
         });
         return;
     }
-    input.classList.remove("is-invalid");
+    inputAsin.classList.remove("is-invalid");
+    letrasValid.classList.remove('active')
+    letrasValid.classList.add('verde')
+
     load.classList.remove("d-none");
+    spinner.classList.add("d-none");
+
     // console.log(getByValue);
     getByAsin(getByValue);
 });
@@ -277,9 +294,13 @@ const getByAsin = async (getByValue) => {
                 "positionClass": "toast-bottom-full-width"
             });
             load.classList.add("d-none");
+            spinner.classList.remove("d-none");
             return;
             //  throw new Error(`Error! status: ${response.status}`);
         }
+
+        load.classList.add("d-none");
+        spinner.classList.remove("d-none");
         lanzarHMTL(data);
     } catch (error) {
         console.log(error);
@@ -302,6 +323,13 @@ function lanzarHMTL(json) {
         title,
         images_flat,
         link,
+        description,
+        feature_bullets_flat,
+        brand,
+        sub_title: {
+            text,
+            link: a
+        },
         buybox_winner: {
             availability: {
                 raw
@@ -311,22 +339,88 @@ function lanzarHMTL(json) {
             price: {
                 value
             }
-        }
-    } = json.product
+        },
+    } = json.product;
     // console.log(raw, value);
+
+    console.log(text, a);
+
 
     USDTOGQT = value * 7.80;
     QT = USDTOGQT.toFixed(3);
     listas.textContent = "";
     const fragment = document.createDocumentFragment();
-    const clone = templates.cloneNode(true);
-    clone.querySelector('.card-img-top').setAttribute('src', images_flat);
-    clone.querySelector('.card-title').textContent = title;
-    clone.querySelector('.badge-pill').textContent = raw ? raw : "Agotado";
-    clone.querySelector('.totals').textContent = isNaN(QT) ? " 00.00" : parse;
-    clone.querySelector('.precioOrig').textContent = value;
-    // // clone.querySelector('.totals').textContent = item.totalReviews;
-    clone.querySelector('.btn-info').setAttribute('href', link);
+    const clone = templateAsin.cloneNode(true);
+    json.product.attributes.forEach(item => {
+        nombre = item.name;
+        valor = item.value;
+        // console.log(nombre, "----------------", valor);
+        clone.querySelector('.attributesname').textContent = nombre;
+        clone.querySelector('.attributesvalue').textContent = valor;
+    })
+
+    clone.querySelector('.img-fluid').setAttribute('src', images_flat);
+    clone.querySelector('.title').textContent = title;
+    clone.querySelector('.text').textContent = text;
+    clone.querySelector('.brand').textContent = brand;
+    clone.querySelector('.badge-pill').textContent = raw ? raw : clone.querySelector('.badge-pill').classList.replace('badge-success', 'badge-danger').textContent = "Agotado";
+    clone.querySelector('.GT').textContent = isNaN(QT) ? " 00.00" : QT;
+    clone.querySelector('.price').textContent = value ? value : "N/D";
+    clone.querySelector('.btn-outline-info').setAttribute('href', link);
+    clone.querySelector('.text').setAttribute('href', a);
+    clone.querySelector('.description').textContent = description;
+    clone.querySelector('.feature_bullets_flat').textContent = feature_bullets_flat;
     fragment.appendChild(clone);
     listas.appendChild(fragment);
+}
+
+
+
+/*------------------------------------POR URL----------------------------*/
+const formURL = document.querySelector('.formURL'),
+    letraURL = document.querySelector('.letraURL'),
+    inputURL = document.querySelector('.inputURL');
+
+formURL.addEventListener('submit', e => {
+    e.preventDefault();
+    getByValue = inputURL.value
+    if (getByValue === "") {
+        inputURL.classList.add("is-invalid");
+        letraURL.classList.add('active')
+        letraURL.classList.remove('verde')
+        toastr.error('Por favor, Ingresa el URL del producto', 'Error', {
+            "positionClass": "toast-top-right"
+        });
+        return;
+    }
+    inputAsin.classList.remove("is-invalid");
+    letraURL.classList.remove('active')
+    letraURL.classList.add('verde')
+
+    // load.classList.remove("d-none");
+    // spinner.classList.add("d-none");
+
+    getByURL(getByValue);
+});
+
+const getByURL = async (getByValue) => {
+    try {
+        const response = await fetch(`https://api.rainforestapi.com/request?api_key=EEE853BDB03E48438A68EF149C575922&type=category&url=${getByValue}`);
+        const data = await response.json();
+        // console.log(data);
+
+        if (response.status === 401) {
+            toastr.error(`Error al cargar, el servidor respondió con un estado de ${response.status}`, `Error: ${response.status}`, {
+                "positionClass": "toast-bottom-full-width"
+            });
+            // load.classList.add("d-none");
+            // spinner.classList.remove("d-none");
+
+            return;
+            //  throw new Error(`Error! status: ${response.status}`);
+        }
+        lanzarHMTL(data);
+    } catch (error) {
+        console.log(error);
+    }
 }
